38c8c66018351ffd2ab9ad8ffceeef2490f8bd1b
Heath Matthew (XC-AS/ENG12-Brg) <meh1brg@pt.bosch.com>
2025-07-25 17:34:21 +0100

    oORanges and catted

--- alongside.py
+++ alongside.py
@@ -1,24 +1,51 @@
 import torch
-from torch import nn
+from torch import nn, Tensor
+from typing import Callable, Tuple, List
 
 class Alongside(nn.Module):
-    def __init__(self, operation, *modules):
+    def __init__(self, operation : Callable, *modules : nn.Module) -> None:
         super().__init__()
-        self.modules = nn.ModuleList(modules)
-        if not self.modules:
+        self.module_list = nn.ModuleList(modules)
+        if not self.module_list:
             raise Exception("At least one module must be passed")
         self.operation = operation
 
-    def forward(self, x):
-        return self.operation([module(x) for module in self.modules])
+    def forward(self, x:Tensor) -> Tensor:
+        return self.operation([module(x) for module in self.module_list])
     
-def stack_modules(*modules):
-    return Alongside(torch.stack, modules)
+def stack_modules(*modules : nn.Module) -> Alongside:
+    return Alongside(torch.stack, *modules)
 
-def cat_modules(*modules):
-    return Alongside(torch.cat, modules)
+def cat_modules(*modules : nn.Module) -> Alongside:
+    return Alongside(torch.cat, *modules)
 
-def sum_modules(*modules):
+def sum_modules(*modules : nn.Module) -> Alongside:
     def sum(x):
         return torch.stack(x).sum(-1)
-    return Alongside(sum, modules)
+    return Alongside(sum, *modules)
+
+class OnRange(nn.Module):
+    def __init__(self, layer: nn.Module, start : int = 0, end : int|None = None) -> None:
+        super().__init__()
+        self.layer = layer
+        self.start = start
+        self.end = end
+
+    def forward(self, x: Tensor) -> Tensor:
+        slice = x[:, self.start:self.end] if self.end is not None else x[:, self.start:]
+        return self.layer(slice)
+
+def catted(*modules_and_lengths : Tuple[nn.Module, int], final: nn.Module|None = None) -> Alongside:
+    starts:List[int] = []
+    sum : int = 0
+    for (_, length) in modules_and_lengths:
+        starts.append(sum)
+        sum += length
+        
+    ends = starts[1:]
+    starts.append(sum)
+    on_ranges = [OnRange(layer, start, end) for (layer, _), start, end in zip(modules_and_lengths, starts, ends)]
+    if final is not None:
+        on_ranges.append(OnRange(final, sum))
+    return cat_modules(*on_ranges)
+     
--- unreliable_connection.py
+++ unreliable_connection.py
@@ -6,6 +6,7 @@ class UnreliableConnectionDummy(nn.Module):
     def __init__(self, probability_model: nn.Module):
         super().__init__()
         self.probability_model = probability_model
+
     
     def forward(self, _) -> torch.Tensor:
         raise Exception( "UnreliableConnectionDummy cannot be called on real data. It is only for code generation")
@@ -22,8 +23,15 @@ class UnreliableConnection(nn.Module):
         samples_per_timeline : int = number_of_samples // self.incoming_timelines
         out : torch.Tensor = torch.zeros((number_of_samples + samples_per_timeline, size_of_row))
         out[:number_of_samples, :] = x
-        probabilities : torch.Tensor = self.probability_model(x)
-        out[:number_of_samples, 0] *= probabilities
+
+        without_probabilities: torch.Tensor = x[:, 1:]
+        probabilities_at_connection  = self.probability_model(without_probabilities)
+        out[:number_of_samples, 0] *= probabilities_at_connection
         out[number_of_samples: , 0] = 1.
         timelines = x.chunk(samples_per_timeline)
-        #TODO. finish
+        
+        for timeline in timelines:
+            out[number_of_samples: , 0] -= timeline[:, 0]
+            
+        return out
+    
